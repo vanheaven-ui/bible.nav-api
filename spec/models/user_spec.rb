bb3d0111facfb920ff589216efb4b0aeb12b182f
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-27 22:48:07 +0300

    Add association User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:favorites).with_foreign_key(:created_by) }
+    it { should_have_many(:comments).withPforeign_key(:written_by) }
+  end
   
   describe 'validations' do
     it { should validate_presence_of(:username) }
