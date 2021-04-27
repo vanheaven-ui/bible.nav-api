b1da36e53bf7efab13382361e51ef712448eecd0
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-27 23:32:21 +0300

    Define global issue_token JWT method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  def jwt_key
+    Rails.application.secrets.secret_key_base
+  end
+
+  def issue_token(user)
+    JWT.encode({ user_id: user.id }, jwt_key, 'hs256')
+  end
 end
