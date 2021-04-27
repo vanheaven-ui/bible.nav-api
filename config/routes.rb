32c7b41cbcbeb872e0b6637a3f503c3666d75992
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-28 00:25:39 +0300

    Define login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  post '/signup', to: 'users#signup'
-  post '/login', to: 'authentication#login'
+  namespace :api do
+    namespace :v1 do
+      post '/signup', to: 'users#signup'
+      post '/login', to: 'authentication#login'
+    end
+  end
 end
