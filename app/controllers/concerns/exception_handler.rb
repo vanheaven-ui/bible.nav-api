a42188589e9876fb992bc8f9e0546b9b9c6b254c
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-28 00:03:24 +0300

    Add custom exceptio errorn

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,17 +1,24 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: { message: e.message }, status: :not_found
     end
 
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
   end
 
   private
 
   def four_twenty_two(e)
     render json: { message: e.message }, status: :unprocessable_entity
+  end
+
+  def unauthorized_request(e)
+    render json: { message: e.message }, status: :unauthorized
   end
 end
