ee3558ee81a57760b0555bfda0a9cb3181c84049
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-27 23:20:29 +0300

    Implement signup action and whitelisted signup params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def signup
+    @user = User.create!(signup_params)
+    @token = issue_token(@user)
+    render json: { user: @user, jwt: @token }, status: :created
+  end
+
+  private
+
+  def signup_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
 end
