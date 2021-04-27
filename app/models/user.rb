352d07ed08a1f5968dd591271b252bc3333247a5
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-27 23:03:46 +0300

    Implement associations and validation of email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates_presence_of :username, :password, :email, :password_confirmation
+  validates_uniqueness_of :username, :email 
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP 
+
+  has_many :favorites, dependent: :destroy, foreign_key: :created_by
+  has_many :comments, dependent: :destroy, foreign_key: :written_by
 end
