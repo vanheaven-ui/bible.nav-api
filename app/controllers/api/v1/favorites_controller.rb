b6eaae3830813f9ff0d5dff62b50952c1903e169
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-30 16:38:24 +0300

    Add favorites resource

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::FavoritesController < ApplicationController
+
+  before_action :set_favorite, only: %i[show destroy]
+
+  def index
+    @favorites = current_user.favorites
+    render json: { favorites: @favorites }
+  end
+  
+  def create
+    @favorite = current_user.favorites.create!(favorite_params)
+    render json: { favorite: @favorite }, status: :created
+  end
+
+  def show
+    render json: { favorite: @favorite}
+    head :no_content
+  end
+
+  def destroy
+    @favorite.destroy
+    head :no_content
+  end
+
+  private 
+
+  def favorite_params
+    params.permit(:book_name, :chapter_num, :verse_num, :verse)
+  end
+
+  def set_favorite
+    @favorite = current_user.favorites.find(favorite_params[:id])
+  end
+end
