aa6f9cad79770530c3d6e879c7af57f13b6520b7
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-04-30 16:39:53 +0300

    Define rest rutes for favorites resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
+  resources :favorites
   root 'home#index'
 
   namespace :api do
     namespace :v1 do
       post '/signup', to: 'users#signup'
       post '/login', to: 'authentication#login'
+      resources :users, only: %i[show] do
+        resources :favorites, only: %i[create show index destroy]
+      end
     end
   end
 end
