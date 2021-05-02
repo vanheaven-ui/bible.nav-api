5fca5844304997423889c4a953e32d9971108686
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-02 22:42:59 +0300

    Update users model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { should have_many(:favorites).with_foreign_key(:created_by) }
+    it { should have_many(:favorites) }
     it { should have_many(:comments).with_foreign_key(:written_by) }
   end
 
