5b465ec10087e5d0a763f5017ed88cb240ddec0e
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-07 16:17:18 +0300

    Enable spec support directory and include spec helper modules

---
+++ spec/support/controller_spec_helper.rb
@@ -0,0 +1,21 @@
+module ControllerSpecHelper
+  MY_SECRET = Rails.application.secrets.secret_key_base
+
+  def token_generator(user_id)
+    JWT.encode({ user_id: user_id }, MY_SECRET)
+  end
+
+  def valid_headers
+    {
+      "Authorization" => token_generator(user.id),
+      "Content-Type" => "application/json"
+    }
+  end
+
+  def invalid_headers
+    {
+      "Authorization": nil,
+      "Content-Type" => "application/json"
+    }
+  end
+end
