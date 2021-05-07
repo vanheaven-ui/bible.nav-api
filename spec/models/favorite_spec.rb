153ce3d4769cf3f714b5a3bf2e0b370316a43033
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-07 16:20:04 +0300

    Write favorite model specs

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should validate_presence_of(:book_name) }
+    it { should validate_presence_of(:chapter_num) }
+    it { should validate_presence_of(:verse_num) }
+    it { should validate_presence_of(:verse) }
+  end
+
+  describe 'validations' do
+    it { should belong_to(:user) }
+  end
 end
