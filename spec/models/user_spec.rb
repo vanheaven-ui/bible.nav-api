8aa90c0de7c0ef164dbb865470b51d21dbc39942
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-07 16:18:27 +0300

    Remove unnecessary association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:favorites) }
-    it { should have_many(:comments).with_foreign_key(:written_by) }
   end
 
   describe 'validations' do
