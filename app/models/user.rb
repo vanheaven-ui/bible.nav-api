d62e61eab4aafee9c8c9e77d89711d14089a314d
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-11 12:09:48 +0300

    Add length validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :username, :email
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates :password, length: { minimum: 6 }
+  validates :username, length: { minimum: 3 }
 
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy, foreign_key: :written_by
