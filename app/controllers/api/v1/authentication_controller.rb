803cbb17ae7d5c937ca2c1b455aeb3a68519ec52
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-13 10:13:22 +0300

    Modify response for authentication#create action

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < ApplicationController
     user = User.find_by!(username: login_params[:username])
     if user.authenticate(login_params[:password])
       token = issue_token(user)
-      render json: { user: user, jwt: token }
+      render json: { user_name: user.username, jwt: token, email:  user.email }
     else
       render json: { error: 'Invalid password' }, status: :unprocessable_entity
     end
--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  skip_before_action :authorized, only: :index
-
-  def index
-    render json: { message: 'Welcome to bible.nav api' }
-  end
-end
